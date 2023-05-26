5cb94736ac764f4ae9b6bd717a560759145cc376
Hernan1Escudero <hernanescudero2@gmail.com>
2023-05-26 06:27:32 -0300

    "arreglo2"

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidget from '../CartWidget/CartWidget';
-import "../NavBar/NavBar.css"
+import "./NavBar.css"
 import { Link } from 'react-router-dom';
 
 function BasicExample() {
